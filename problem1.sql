8fad4f32c10c10e1db6f84219b4e1f2f90690950
Michael Smith <michaelsmith1994@yahoo.com>
2024-12-12 15:01:58 +0000

    another lab completed

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+select * from class
+left outer join student on student.class_title = class.class_title;
+
---
+++ problem2.sql
@@ -0,0 +1,2 @@
+select * from class
+right outer join textbook on textbook.class_title = class.class_title;
